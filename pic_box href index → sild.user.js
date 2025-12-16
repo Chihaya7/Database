a825c0238a6b9bf5e113de18d2546fdc38b1c47f
Chihaya <44675983+Chihaya7@users.noreply.github.com>
2025-12-16 20:17:32 +0800

    Update pic_box href index → sild.user.js

--- pic_box href index → sild.user.js
+++ pic_box href index → sild.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         pic_box href index → silde
 // @namespace    https://tampermonkey.net/
-// @version      2025-12-16 11:03:32
+// @version      2025-12-16 11:03:33
 // @icon         https://wnacg.com/favicon.ico
 // @description  Replace "index" with "silde" in hrefs inside .pic_box
-// @match        https://www.wnacg.*
+// @match        https://*.wnacg.ru/*
+// @match        https://*.wnacg.com/*
 // @match        https://www.wn04.ru/*
 // @match        https://www.wn05.ru/*
 // @match        https://www.wnacg05.cc/*
